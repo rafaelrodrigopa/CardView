c66f910a799401a96821b6fc1236467084eca6d7
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-13 22:04:04 -0300

    Configuring AdapterView - Parte 2

--- app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cardview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,32 +9,46 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cardview.R;
+
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        // View
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.postagem_detalhe, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        holder.textNome.setText("Rafael Rodrigo");
+        holder.textPostagem.setText("#tbt Viagem legal!");
+        holder.imgPostagem.setImageResource(R.drawable.imagem1);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imgPostagem;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            textNome = itemView.findViewById(R.id.textNome);
+            textPostagem = itemView.findViewById(R.id.textPostagem);
+            imgPostagem = itemView.findViewById(R.id.imgPostagem);
         }
     }
 }
