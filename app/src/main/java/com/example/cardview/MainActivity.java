59cb27e8ae8cac6e4c0b277fc3dfbb0c80442466
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-13 21:14:00 -0300

    Project CardView created

---
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
